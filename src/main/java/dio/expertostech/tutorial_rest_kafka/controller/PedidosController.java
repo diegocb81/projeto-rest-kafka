35ebd9b0d574944a676ef12e3836de6204cf6d51
Diego Barcellos <diegocb81@gmail.com>
2024-08-22 23:04:03 -0300

    Testes iniciais da aplicação

--- src/main/java/dio/expertostech/tutorial_rest_kafka/controller/PedidosController.java
+++ src/main/java/dio/expertostech/tutorial_rest_kafka/controller/PedidosController.java
@@ -1,14 +1,23 @@
 package dio.expertostech.tutorial_rest_kafka.controller;
 
+import dio.expertostech.tutorial_rest_kafka.data.PedidoData;
+import dio.expertostech.tutorial_rest_kafka.services.RegistraEventoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PedidosController {
 
-    public ResponseEntity<String> SalvarPedido() {
+    private final RegistraEventoService eventoService;
 
-
+    @PostMapping(path = "/api/salva-pedido")
+    public ResponseEntity<String> SalvarPedido(@RequestBody PedidoData pedido) {
+        eventoService.adicionarEvento("SalvarPedido", pedido);
         return ResponseEntity.ok("Sucesso");
     }
 }
