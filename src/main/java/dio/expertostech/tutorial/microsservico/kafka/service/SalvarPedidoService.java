586b7f800234a6e8ae78b285e49ebe49c0f0253c
Diego Barcellos <diegocb81@gmail.com>
2024-08-24 00:20:14 -0300

    Desenvolvimento do serviço de consumidor

---
+++ src/main/java/dio/expertostech/tutorial/microsservico/kafka/service/SalvarPedidoService.java
@@ -0,0 +1,11 @@
+package dio.expertostech.tutorial.microsservico.kafka.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalvarPedidoService {
+
+    private void executar() {
+
+    }
+}
